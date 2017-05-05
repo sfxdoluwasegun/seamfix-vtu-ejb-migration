2a86a719933ceb047387a69d5c219be68791bf01
Ezewuzie Okafor <eokafor@seamfix.com>
2017-05-05 17:48:00 +0100

    added authentication to the vtu soap service

--- src/main/java/com/sf/vas/mtnvtu/service/VtuMtnSoapService.java
+++ src/main/java/com/sf/vas/mtnvtu/service/VtuMtnSoapService.java
@@ -5,10 +5,12 @@ package com.sf.vas.mtnvtu.service;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.Map;
 
 import javax.annotation.PostConstruct;
 import javax.ejb.Singleton;
 import javax.inject.Inject;
+import javax.xml.ws.BindingProvider;
 
 import org.jboss.logging.Logger;
 
@@ -16,6 +18,8 @@ import com.sf.vas.mtnvtu.enums.VtuMtnSetting;
 import com.sf.vas.mtnvtu.soapartifacts.HostIFService;
 import com.sf.vas.mtnvtu.soapartifacts.HostIFServicePortType;
 import com.sf.vas.mtnvtu.tools.VtuMtnQueryService;
+import com.sf.vas.utils.crypto.EncryptionUtil;
+import com.sf.vas.utils.exception.VasException;
 import com.sf.vas.utils.exception.VasRuntimeException;
 
 /**
@@ -29,11 +33,14 @@ public class VtuMtnSoapService {
 	private Logger log = Logger.getLogger(getClass());
 	
 	@Inject
-	VtuMtnQueryService vtuQueryService;
+	private VtuMtnQueryService vtuQueryService;
 	
 	private HostIFService hostIFService;
 	
 	private HostIFServicePortType hostIFServicePortType;
+	
+	@Inject
+	private EncryptionUtil encryptionUtil;
 	
 	@PostConstruct
 	private void init() {
@@ -65,6 +72,31 @@ public class VtuMtnSoapService {
 		}
 		
 		hostIFServicePortType = hostIFService.getHostIFServiceSOAP11PortHttp();
+		
+		
+		String sUsername = vtuQueryService.getSettingValue(VtuMtnSetting.VTU_VEND_USERNAME);
+		String sPassword = vtuQueryService.getSettingValue(VtuMtnSetting.VTU_VEND_PASSWORD);
+		
+		if(sUsername == null || sUsername.trim().isEmpty() || sPassword == null || sPassword.trim().isEmpty()){
+			throw new VasRuntimeException("vend user name or password not configured"); 
+		}
+		
+		String username;
+		String password;
+		try {
+			username = encryptionUtil.decrypt(sUsername);
+			password = encryptionUtil.decrypt(sPassword);
+		} catch (VasException e) {
+			throw new VasRuntimeException("error decrypting configured vend user name or password", e);
+		}
+		
+		BindingProvider bindingProvider = (BindingProvider) hostIFServicePortType;
+		
+		Map<String, Object> requestContext = bindingProvider.getRequestContext();
+		
+		requestContext.put(BindingProvider.USERNAME_PROPERTY, username);
+		requestContext.put(BindingProvider.PASSWORD_PROPERTY, password);
+	
 	}
 
 	/**
