9501960d2459a31454199d6c26767d6351be6ec4
Ezewuzie Okafor <eokafor@seamfix.com>
2017-07-09 22:50:36 +0100

    used a DTO to guide vending of airtime in the vtu module #AT-395

---
+++ src/main/java/com/sf/vas/mtnvtu/dto/AirtimeTransferRequestDTO.java
@@ -0,0 +1,43 @@
+/**
+ * 
+ */
+package com.sf.vas.mtnvtu.dto;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+
+import com.sf.vas.atjpa.entities.NetworkCarrier;
+import com.sf.vas.atjpa.entities.Subscriber;
+import com.sf.vas.atjpa.entities.TopUpProfile;
+import com.sf.vas.atjpa.entities.TopupHistory;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * @author dawuzi
+ *
+ */
+@Getter
+@Setter
+@ToString
+public class AirtimeTransferRequestDTO implements Serializable {
+
+	private static final long serialVersionUID = -3674156609057897222L;
+
+	private String msisdn;
+	
+	private BigDecimal amount;
+	
+	private Subscriber subscriber;
+	
+	private NetworkCarrier networkCarrier;
+	
+	private TopUpProfile topUpProfile;
+	
+	private String callbackUrl;
+
+	private TopupHistory topupHistory;
+
+}
